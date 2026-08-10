e2b3f973bb8aefd747265919f47076af71236ce3
Théo Souza <135135018+TheoSouzaSantos@users.noreply.github.com>
2026-08-10 19:54:56 -0300

    Segurança
    Removed sensitive Firebase configuration details.

--- services/firebaseConfig.js
+++ services/firebaseConfig.js
@@ -4,12 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDDB4wns3Irkrsm1xwft8AX1iYecAWjFBk",
-    authDomain: "dynamic-flame-sensor-cb50b.firebaseapp.com",
-    projectId: "dynamic-flame-sensor-cb50b",
-    storageBucket: "dynamic-flame-sensor-cb50b.firebasestorage.app",
-    messagingSenderId: "638183225251",
-    appId: "1:638183225251:web:43b291d8f2aceec27d8c16"
+    
   };
   
 
